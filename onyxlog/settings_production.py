1332a74ad638e045234b6fc7a94f9c45d40ff85e
JAIR VERCOSA <jairvercosa@gmail.com>
2014-12-27 03:12:02 -0200

    api saldos

--- onyxlog/settings_production.py
+++ onyxlog/settings_production.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'bootstrap_toolkit',
     'django_bootstrap_breadcrumbs',
+    'rest_framework',
     
     'onyxlog.core',
     'onyxlog.acesso',
@@ -79,6 +80,14 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.messages.context_processors.messages",
     "django.core.context_processors.request",
 )
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    #'DEFAULT_PERMISSION_CLASSES': [
+    #    'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    #]
+}
 
 LOGGING = {
     'version': 1,
